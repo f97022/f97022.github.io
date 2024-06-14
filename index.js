eacc25e14cf3f6be93379164c40147f5845293ec
f97022 <f97022@gmail.com>
2024-06-14 19:26:38 +0800

    手機RWD測試 20210614
    只剩media設定未完成

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const tab = document.querySelector('#tab')
 const content = document.querySelector('#content')
 
-// 取得導覽列的位移位置
-const sticky = tab.offsetTop
-//當頁面捲動時 如果超過導覽列的位置則會給予sticky樣式 否則移除sticky樣式
-window.onscroll = function stickyNav() {
-  if (window.scrollY >= sticky) {
-    tab.classList.add('sticky')
-  } else {
-    tab.classList.remove('sticky')
-  }
-}
-
 tab.addEventListener('click', function switchContent(event) {
   //取得class有active的元素
   const activeItem = document.querySelector('#tab .active')
